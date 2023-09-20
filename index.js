be9ccc8ad7db16e456945c074e55d2a31507aa3e
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2023-09-20 19:11:54 +0500

    fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const container2 = document.querySelector(".container-2");
   const moreBtn = document.querySelector(".btn");
   const arrow = document.querySelector(".arrow");
+  const toMainPageBtn = document.querySelector("#toMainPage");
 
   //появление стрелки при наведении на кнопку
   moreBtn.addEventListener("mouseenter", () => {
@@ -12,16 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
     arrow.classList.remove("showArrow");
   });
 
-  //скролл при нажатии на кнопку
+  //перемещение на страницу доп. информации
   moreBtn.addEventListener("click", () => {
-    // window.scrollBy({
-    //   top: window.innerHeight,
-    //   behavior: "smooth",
-    // });
     container.style.opacity = "0";
+
     setTimeout(() => {
       container.style.display = "none";
       container2.style.display = "flex";
+
       setTimeout(() => {
         container2.style.opacity = "1";
         container2.style.transform = "translateX(0)";
@@ -29,16 +28,16 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 300);
   });
 
-  //скролл на главную
-  const toMainPageBtn = document.querySelector("#toMainPage");
+  //перемещение на главную
   toMainPageBtn.addEventListener("click", () => {
     container2.style.transform = "translateX(-300%)";
+
     setTimeout(() => (container2.style.display = "none"), 150);
+
     setTimeout(() => {
       container.style.display = "flex";
       container.style.transform = "transform: translateY(-300%)";
       container.style.opacity = "1";
     }, 300);
-    console.log(container, container2);
   });
 });
